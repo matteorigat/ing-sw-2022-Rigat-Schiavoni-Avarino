c67a4ff1456021638119d07221d5001dc068b30e
Matteo Rigat <matteo.rigat@gmail.com>
2022-03-08 15:38:49 +0100

    initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
         System.out.println("mefiu stolto");
 
     }
