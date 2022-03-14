787fa8224a6b7fe2d34a90d9e26f827a16adc189
Matteo Rigat <matteo.rigat@gmail.com>
2022-03-14 15:16:01 +0100

    Methods first implementation

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,4 +1,39 @@
 package it.polimi.ingsw.model;
 
+import java.util.ArrayList;
+
+/*
+ * In this class we manage the main actions of the match.
+ */
+
 public class Game {
+    private final ArrayList<Player> players;
+    private final GameBoard gameBoard;
+
+
+    //Constructor Game creates a new Game instance
+    public Game() {
+        players = new ArrayList<>();
+        gameBoard = new GameBoard();
+    }
+
+    //Gets the players of the match
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    //Gets a player by nickname
+    public Player getPlayer(String nickname) throws NotExistingPlayerException{
+        for (Player p:players) {
+            if(p.getNickname().equals(nickname))
+                return  p;
+        }
+        throw  new NotExistingPlayerException();
+    }
+
+    //Gets the gameBoard instance
+    public GameBoard getGameBoard() {
+        return gameBoard;
+    }
+
 }
--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,4 +1,24 @@
 package it.polimi.ingsw.model;
 
 public class Player {
+    private final String nickname;
+    private final SchoolBoard playerSchoolBoard;
+
+
+    //Constructor Player creates a new Player instance
+    public Player(String nickname, SchoolBoard playerSchoolBoard) {
+        this.nickname = nickname;
+        this.playerSchoolBoard = playerSchoolBoard;
+    }
+
+    //Gets the nickname of the player
+    public String getNickname() {
+        return nickname;
+    }
+    //Gets the schoolBoard of the player
+    public SchoolBoard getPlayerSchoolBoard() {
+        return playerSchoolBoard;
+    }
 }
+
+
