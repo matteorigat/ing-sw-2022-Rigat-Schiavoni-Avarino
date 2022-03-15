e4bb8f9c9c570f6f12b50d338d84e642849cc048
nicoloavarino <nicolo.avarino@mail.polimi.it>
2022-03-15 14:01:06 +0100

    Methods first implementation

--- src/main/java/it/polimi/ingsw/model/gameboard/GameBoard.java
+++ src/main/java/it/polimi/ingsw/model/gameboard/GameBoard.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 public class GameBoard {
 
-    private final LinkedList<Island> islands;
+    private final LinkedList<Island> islands1;
     private final ArrayList<Cloud> clouds;
     private final MotherNature motherNature;
     private final Bag bag;
