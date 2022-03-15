eabd3463d05a90162e33ac9a579e3a4044081628
nicoloavarino <nicolo.avarino@mail.polimi.it>
2022-03-15 16:43:37 +0100

    Island

--- src/main/java/it/polimi/ingsw/model/gameboard/Island.java
+++ src/main/java/it/polimi/ingsw/model/gameboard/Island.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.model.gameboard;
 
+import it.polimi.ingsw.model.Student;
+
+import java.util.ArrayList;
+
 public class Island {
     private int IslandRank;
     private int Tower;
+    private ArrayList<Student> students;
+    private int[] NumStudents;
+
+
 
 
 
