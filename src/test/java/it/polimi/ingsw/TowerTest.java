adfd58e8fbf77add25caa4f0332c3d8ffbf56987
Giuseppe <giuseppe1.schiavoni@mail.polimi.it>
2022-04-03 19:08:30 +0200

    Test

--- src/test/java/it/polimi/ingsw/TowerTest.java
+++ src/test/java/it/polimi/ingsw/TowerTest.java
@@ -1,4 +1,15 @@
 package it.polimi.ingsw;
+import it.polimi.ingsw.model.Tower;
+import it.polimi.ingsw.model.enumeration.Colour;
+import it.polimi.ingsw.model.enumeration.TowerColour;
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
-public class TowerTest {
+public class TowerTest extends TestCase{
+
+    @Test
+    public void testTower(){
+        Tower tower = new Tower(TowerColour.Black);
+        assertEquals(tower.getTowerColor(), TowerColour.Black);
+    }
 }
