1c7b18fa1b6d340dbf7e8004f3f49d9289c72e84
nicoloavarino <nicolo.avarino@mail.polimi.it>
2022-04-03 19:23:24 +0200

    ProfessorTest

--- src/test/java/it/polimi/ingsw/TowerTest.java
+++ src/test/java/it/polimi/ingsw/TowerTest.java
@@ -10,6 +10,10 @@ public class TowerTest extends TestCase {
     public void testTower(){
         Tower tower = new Tower(TowerColour.Black);
         assertEquals(TowerColour.Black,tower.getTowerColor());
+        Tower tower1 = new Tower(TowerColour.White);
+        assertEquals(TowerColour.White,tower1.getTowerColor());
+        Tower tower2 = new Tower(TowerColour.Grey);
+        assertEquals(TowerColour.Grey,tower2.getTowerColor());
 
     }
 
