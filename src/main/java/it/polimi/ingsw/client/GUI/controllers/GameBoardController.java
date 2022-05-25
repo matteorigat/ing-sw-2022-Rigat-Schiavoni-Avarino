00da41c55fac2f206fcf299d20e853c9dc06f540
Matteo Rigat <matteoluigi.rigat@mail.polimi.it>
2022-05-25 16:07:41 +0200

    GameBoard gui

--- src/main/java/it/polimi/ingsw/client/GUI/controllers/GameBoardController.java
+++ src/main/java/it/polimi/ingsw/client/GUI/controllers/GameBoardController.java
@@ -4,18 +4,24 @@ import it.polimi.ingsw.client.ClientAppGUI;
 import it.polimi.ingsw.client.GUI.ClientGUI;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseDragEvent;
 
 public class GameBoardController {
 
     private ClientAppGUI gui;
 
-    @FXML private TextField ip;
-    @FXML private TextField port;
-
+    private int dragItem;
 
     @FXML
-    protected void onButtonClick() {
+    protected void selectStudentGreen() {
+         dragItem = 0;
+        System.out.println("stai gdraggando");
 
+    }
+
+    @FXML
+    protected void diningRoomGreen() {
+       System.out.println("hai spostato nella sala verde il colore: " + dragItem);
 
     }
 
