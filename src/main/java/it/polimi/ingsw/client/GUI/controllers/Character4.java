fdd9d4808377977d17e8d47bef1aec1f481a4412
nicoloavarino <nicolo.avarino@mail.polimi.it>
2022-06-10 00:13:52 +0200

    first implemention gui character 3,4,6,8

---
+++ src/main/java/it/polimi/ingsw/client/GUI/controllers/Character4.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.client.GUI.controllers;
+
+import it.polimi.ingsw.client.ClientAppGUI;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Character4 {
+    private ClientAppGUI gui;
+
+    @FXML
+    private Label effect;
+
+    @FXML
+    protected void onButtonClick() {
+        Platform.runLater(()-> gui.getClientGUI().asyncWriteToSocket("100,2"));
+    }
+
+
+    public void setGui(ClientAppGUI gui) {
+        this.gui = gui;
+    }
+}
+
+
