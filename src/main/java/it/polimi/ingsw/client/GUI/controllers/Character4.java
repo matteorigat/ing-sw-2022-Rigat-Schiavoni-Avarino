1d08a26596254bb627141701c283c007042c8f7c
nicoloavarino <nicolo.avarino@mail.polimi.it>
2022-06-10 00:15:38 +0200

    first implemention gui character 3,4,6,8

--- src/main/java/it/polimi/ingsw/client/GUI/controllers/Character4.java
+++ src/main/java/it/polimi/ingsw/client/GUI/controllers/Character4.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Character4 {
-    private ClientAppGUI gui;
+    private ClientAppGUI gui ;
 
     @FXML
     private Label effect;
