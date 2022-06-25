521cf8627c00c5c2b54bf414d133ba6d186621e3
Matteo Rigat <matteoluigi.rigat@mail.polimi.it>
2022-06-25 18:35:12 +0200

    ..

--- src/test/java/it/polimi/ingsw/controller/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -4,6 +4,12 @@ import it.polimi.ingsw.model.Parameters;
 
 import static org.junit.Assert.*;
 
+import it.polimi.ingsw.model.enumeration.Colour;
+import it.polimi.ingsw.model.gameboard.Island;
+import it.polimi.ingsw.model.gameboard.characters.Character2;
+import it.polimi.ingsw.model.gameboard.characters.Character6;
+import it.polimi.ingsw.model.gameboard.characters.Character8;
+import it.polimi.ingsw.model.gameboard.characters.CharacterCard;
 import it.polimi.ingsw.model.player.Player;
 import org.junit.Test;
 
@@ -96,83 +102,105 @@ public class ControllerTest {
         controller.playAssistantCard(0,1);
         assertEquals(0, controller.getCurrentPlayer());
 
-        /*
+
+
+
+        controller.getModel().print("mef");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         for(int i=0; i<50; i++){
-            game.getPlayers().get(0).addCoin();
+            controller.getPlayers().get(0).addCoin();
         }
-        for (CharacterCard c: game.getGameBoard().getThreeCharacterCards()){
+        for (CharacterCard c: controller.getGameBoard().getThreeCharacterCards()){
             if(c.getIndex() == 1){
-                for(Island isl: game.getGameBoard().getIslands()){
+                for(Island isl: controller.getGameBoard().getIslands()){
                     if (isl.getStudents().size() == 1){
-                        if(game.playCharacterCard1(0,1,0,isl.getIslandIndex()) == 1){
+                        if(controller.playCharacterCard1(0,1,0,isl.getIslandIndex()) == 1){
                             assertEquals(2, isl.getStudents().size());
                             assertEquals(2, c.getCost());
-                        } else if(game.playCharacterCard1(0,1,1,isl.getIslandIndex()) == 1){
+                        } else if(controller.playCharacterCard1(0,1,1,isl.getIslandIndex()) == 1){
                             assertEquals(2, isl.getStudents().size());
                             assertEquals(2, c.getCost());
-                        } else if(game.playCharacterCard1(0,1,2,isl.getIslandIndex()) == 1){
+                        } else if(controller.playCharacterCard1(0,1,2,isl.getIslandIndex()) == 1){
                             assertEquals(2, isl.getStudents().size());
                             assertEquals(2, c.getCost());
-                        } else if(game.playCharacterCard1(0,1,3,isl.getIslandIndex()) == 1){
+                        } else if(controller.playCharacterCard1(0,1,3,isl.getIslandIndex()) == 1){
                             assertEquals(2, isl.getStudents().size());
                             assertEquals(2, c.getCost());
-                        } else if(game.playCharacterCard1(0,1,4,isl.getIslandIndex()) == 1){
+                        } else if(controller.playCharacterCard1(0,1,4,isl.getIslandIndex()) == 1){
                             assertEquals(2, isl.getStudents().size());
                             assertEquals(2, c.getCost());
                         }
                         break;
                     }
                 }
-            } else if(c.getIndex() == 3){
-                Colour profColor = game.getPlayers().get(0).getPlayerSchoolBoard().getProfessors().get(0).getProfessorColour();
-                for(Island isl: game.getGameBoard().getIslands()){
+            }
+            /*else if(c.getIndex() == 2){
+                assertFalse(((Character2) c).isEffectFlag());
+                assertEquals(1, controller.playCharacterCard2(0,2));
+                assertTrue(((Character2) c).isEffectFlag());
+            }*/
+            else if(c.getIndex() == 3){
+                Colour profColor = controller.getPlayers().get(0).getPlayerSchoolBoard().getProfessors().get(0).getProfessorColour();
+                for(Island isl: controller.getGameBoard().getIslands()){
                      if(isl.getStudents().size() == 1 && isl.getStudents().get(0).getColour() == profColor){
-                         int numIsland = game.getGameBoard().getIslands().size();
-                         assertEquals(1, game.playCharacterCard3(0,3, isl.getIslandIndex()));
+                         int numIsland = controller.getGameBoard().getIslands().size();
+                         assertEquals(1, controller.playCharacterCard3(0,3, isl.getIslandIndex()));
                          assertEquals(4, c.getCost());
-                         numIsland -= game.getGameBoard().getIslands().size();
+                         numIsland -= controller.getGameBoard().getIslands().size();
                          numIsland = isl.getIslandIndex()-numIsland;
                          if(numIsland < 0){
                              int cont = 0;
                              while (numIsland != 0)
                                  cont++;
-                             numIsland = game.getGameBoard().getIslands().size()-cont;
+                             numIsland = controller.getGameBoard().getIslands().size()-cont;
                          }
-                         assertEquals(game.getPlayers().get(0).getPlayerSchoolBoard().getTowerColor(), game.getGameBoard().getIslands().get(numIsland).getTowerColor());
+                         assertEquals(controller.getPlayers().get(0).getPlayerSchoolBoard().getTowerColor(), controller.getGameBoard().getIslands().get(numIsland).getTowerColor());
                          break;
                      }
                  }
             } else if(c.getIndex() == 4){
-                color = game.getPlayers().get(game.getCurrentPlayer()).getPlayerSchoolBoard().getStudentsEntrance().get(0).getColour().ordinal();
-                game.moveStudentToDiningRoom(0, color);
-                color = game.getPlayers().get(game.getCurrentPlayer()).getPlayerSchoolBoard().getStudentsEntrance().get(0).getColour().ordinal();
-                game.moveStudentToDiningRoom(0, color);
-                color = game.getPlayers().get(game.getCurrentPlayer()).getPlayerSchoolBoard().getStudentsEntrance().get(0).getColour().ordinal();
-                game.moveStudentToDiningRoom(0, color);
-                mn = game.getGameBoard().getMotherNature();
-                assertEquals(-1, game.moveMotherNature(0,3));
-                assertEquals(1, game.playCharacterCard4(0, 4));
-                assertEquals(1, game.moveMotherNature(0,3));
-                assertEquals((mn + 3)%game.getGameBoard().getIslands().size(), game.getGameBoard().getMotherNature());
+                color = controller.getPlayers().get(controller.getCurrentPlayer()).getPlayerSchoolBoard().getStudentsEntrance().get(0).getColour().ordinal();
+                controller.moveStudentToDiningRoom(0, color);
+                color = controller.getPlayers().get(controller.getCurrentPlayer()).getPlayerSchoolBoard().getStudentsEntrance().get(0).getColour().ordinal();
+                controller.moveStudentToDiningRoom(0, color);
+                color = controller.getPlayers().get(controller.getCurrentPlayer()).getPlayerSchoolBoard().getStudentsEntrance().get(0).getColour().ordinal();
+                controller.moveStudentToDiningRoom(0, color);
+                mn = controller.getGameBoard().getMotherNature();
+                assertEquals(-1, controller.moveMotherNature(0,3));
+                assertEquals(1, controller.playCharacterCard4(0, 4));
+                assertEquals(1, controller.moveMotherNature(0,3));
+                assertEquals((mn + 3)%controller.getGameBoard().getIslands().size(), controller.getGameBoard().getMotherNature());
                 assertEquals(2, c.getCost());
 
             } else if(c.getIndex() == 5){
-                 assertEquals(0, game.getGameBoard().getIslands().get(0).getNoEntry());
-                 assertEquals(1, game.playCharacterCard5(0,5,0));
-                 assertEquals(1, game.getGameBoard().getIslands().get(0).getNoEntry());
-                 //come lo testo senza cabiare metà codice? si fa prima a testare dalla view
-            } else if(c.getIndex() == 6){
+                 assertEquals(0, controller.getGameBoard().getIslands().get(0).getNoEntry());
+                 assertEquals(1, controller.playCharacterCard5(0,5,0));
+                 assertEquals(1, controller.getGameBoard().getIslands().get(0).getNoEntry());
+            }/* else if(c.getIndex() == 6){
                  assertFalse(((Character6) c).isEffectFlag());
-                 assertEquals(1, game.playCharacterCard6(0,6));
+                 assertEquals(1, controller.playCharacterCard6(0,6));
                  assertTrue(((Character6) c).isEffectFlag());
-                //come lo testo senza cabiare metà codice? si fa prima a testare dalla view
             } else if(c.getIndex() == 8){
                 assertFalse(((Character8) c).isEffectFlag());
-                //assertEquals(1, game.playCharacterCard6(0,6));
+                assertEquals(1, controller.playCharacterCard6(0,6));
                 assertTrue(((Character8) c).isEffectFlag());
-                //come lo testo senza cabiare metà codice? si fa prima a testare dalla view
-            }
-        }*/
+            }*/
+        }
 
     }
 }
